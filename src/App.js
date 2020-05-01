74d7fc52f0284cae643477382ff458dc59e21a52
rezamike <michael.sanaiha@gmail.com>
2020-05-01 00:38:48 -0700

    push live

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import './App.css';
-import background from './Milkyway_Panorama True.jpg';
 
 const styles = {
-  header: {
-    backgroundImage: `url(${background})`
+  main: {
+    fontSize: "3em",
+    margin: "0 auto"
+  },
+  sub: {
+    fontSize: "1em"
+  },
+  body: {
+    fontSize: "1.5em",
+    width: "75%",
+    margin: "8% auto auto auto"
   }
 };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header" style={styles.header}>
-        
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div style={styles.text}>
+            <p style={styles.main}>cliffside pictures</p>
+            <p style={styles.sub}>EST. 2009</p>
+            <p style={styles.body}>Video production content for the new age - from here to the stars...</p>
+          </div>
+          <a href="mailto:michael.sanaiha@gmail.com?subject=Cliffside Pictures Inquiry" className="button">CONTACT US</a>
+        </div>
+      </div>
+    )
+  };
 }
 
 export default App;
